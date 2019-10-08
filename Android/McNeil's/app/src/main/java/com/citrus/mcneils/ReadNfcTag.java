49d519e0b0fef73bebc91c2fbc328abaa052b784
Kameel <kameel.ahamed@gmail.com>
2019-10-07 20:58:53 -0700

    begun creating ui

---
+++ Android/McNeil's/app/src/main/java/com/citrus/mcneils/ReadNfcTag.java
@@ -0,0 +1,14 @@
+package com.citrus.mcneils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadNfcTag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_nfc_tag);
+    }
+}
